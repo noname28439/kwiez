fe7d006cee408c7fc43748e0398394c0539848ab
Noname <62444462+noname28439@users.noreply.github.com>
2024-03-14 14:06:11 +0100

    fallback route

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ use warp::http::Uri;
 async fn main() {
 
     let public_route = warp::any().and(warp::fs::dir("./frontend/public"));
-    //let fallback_route = warp::any().map(|| warp::redirect(Uri::from_static("/public/index.html")));
+    let fallback_route = warp::any().map(|| warp::redirect(Uri::from_static("/index.html")));
 
-    let routes = public_route;
+    let routes = public_route.or(fallback_route);
+
+
 
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
