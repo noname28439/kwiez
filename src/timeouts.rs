d56f071c0f3818b36cffa2f205e4c3b857bcd3d9
Noname <62444462+noname28439@users.noreply.github.com>
2024-03-20 17:17:30 +0100

    fix

---
+++ src/timeouts.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::thread::spawn;
+use std::time::Duration;
+use tokio::time::sleep;
+
+pub struct TimeoutManager{
+
+}
+
+impl TimeoutManager{
+
+}
