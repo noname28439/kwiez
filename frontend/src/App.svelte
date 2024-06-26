454e13f7805f86225c55fb4b7b2283e1f81f3e57
KrawattenFreak <69595339+KrawattenFreak@users.noreply.github.com>
2024-06-26 22:32:18 +0200

    Updated frontend
    Frontend like a rockband

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -17,9 +17,8 @@
 
 <main>
     <div id="wrapper">
-
         {#await promise}
-            <p>Laden</p>
+            <div class="loader"></div>
         {:then {stats, ranking, cq, statsRes}}
 
             {#if stats}
@@ -48,16 +47,24 @@
             </div>
 
         {/await}
+
+
+    </div>
+    <div class="footer">
+        <p style="font-weight: 800; font-family: 'Manrope', sans-serif; color: #d5d5d5">powered by Odin</p>
+
     </div>
 </main>
 
 <style>
-    main {
-        margin: 1em 1em 0;
+
+    #wrapper {
+        padding: 1em 1em;
+        overflow: hidden;
     }
 
     #EventProgress {
-        margin-top: 2em;
+        margin-top: 1em;
     }
 
     #Content {
@@ -68,7 +75,7 @@
 
     @media (min-width: 640px) {
         main {
-            margin: 1em 0 0;
+            /*margin: 1em 0 0;*/
             width: 100%;
             justify-content: center;
         }
@@ -77,7 +84,41 @@
             max-width: 640px;
             margin-left: auto;
             margin-right: auto;
+
+            padding: 1em 3em;
+            overflow-x: hidden;
         }
+
+
+    }
+
+    .loader {
+        border: 8px solid #f3f3f3; /* Light grey */
+        border-top: 8px solid #acacac; /* Blue */
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        animation: spin 2s linear infinite;
+        display: flex;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+
+
+    .footer {
+
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+        display: flex;
+        justify-content: center;
+        margin-bottom: 1em;
+
     }
 
 </style>
